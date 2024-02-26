500557aef348a1d7eb5852490911d19e5f58a25e
mukeshkumarbagaria <mkbagaria13@gmail.com>
2024-02-27 00:06:47 +0530

    basic content updation

---
+++ .next/static/webpack/pages/index.f9d9385f5cf40761.hot-update.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("pages/index",{
+
+/***/ "./src/data/sections/about.json":
+/*!**************************************!*\
+  !*** ./src/data/sections/about.json ***!
+  \**************************************/
+/***/ (function(module, __unused_webpack_exports, __webpack_require__) {
+
+module.exports = JSON.parse('{"title":"A Design Agency <br/>Delivering Success by <br/>Winning Hearts","subtitle":"Welcome to Technovita Solution","number":{"value":"5","label":"Years of Digital Solutions Experience"},"items":[{"title":"Our Mission","text":"From the moment our company was founded we have helped our clients find <strong>exceptional solutions for their businesses</strong> memorable brands and digital products. Our expertise grows with each year, and our accumulated experience."},{"title":"Our Goal","text":"Our goal is to deliver amazing experiences that make people talk, and build strategic value for brands, tech, entertainment.","button":{"label":"More About Us","link":"/about"}}]}');
+
+/***/ })
+
+});
